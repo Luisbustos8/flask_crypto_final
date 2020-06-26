006506b299788c267714d727e33f6e2a3c341349
Luis Alejandro Bustos Lucas <Luis@MacBook-Pro-de-Luis-5.local>
2020-06-26 11:31:04 +0200

    Creación primera pantalla

--- routes.py
+++
@@ -1,7 +0,0 @@
-from my_app import app
-from flask import render_template
-
-
-@app.route("/")
-def index():
-    return "Aquí irán mis movimientos"
--- run.py
+++ run.py
@@ -1,4 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-from my_app import routes
+import sqlite3
+
+
+@app.route("/")
+def index():
+
+    data_base = "./data/movimientos.db"
+
+    try:   
+        conn = sqlite3.connect(data_base)
+        cur = conn.cursor()
+
+        query = "SELECT * FROM movimientos;"
+        movimientos = cur.execute(query).fetchall()
+        print(movimientos)
+
+        if movimientos[0] == None:
+            return "No hay movimientos"
+        else:
+            conn.close()
+            return render_template("index.html", movimientos=movimientos)
+            
+    except:
+        return render_template("index.html")
+   
+
+
+@app.route("/purchase")
+def purchase():
+    return "Aquí ira la compra de cryptos"
